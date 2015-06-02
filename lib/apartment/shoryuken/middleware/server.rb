a7ea9c5d4758a8956ec49cc9de10f09451f4abe1
yurifrl <yurifrl@hotmail.com>
2015-06-02 11:52:02 -0300

    test with switch

--- lib/apartment/shoryuken/middleware/server.rb
+++ lib/apartment/shoryuken/middleware/server.rb
@@ -1,9 +1,7 @@
 module Apartment::Shoryuken::Middleware
   class Server
     def call(worker_instance, queue, sqs_msg, body)
-      Apartment::Tenant.switch(body['apartment']) do
-        yield
-      end
+      Apartment::Tenant.switch!(body['apartment'])
     end
   end
 end
