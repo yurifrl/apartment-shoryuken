d04c1e83b37c4de0b57b68d13d9c121a7d0b20a8
yurifrl <yurifrl@hotmail.com>
2015-06-02 11:52:25 -0300

    test with switch

--- lib/apartment/shoryuken/middleware/client.rb
+++ lib/apartment/shoryuken/middleware/client.rb
@@ -1,7 +1,7 @@
 module Apartment::Shoryuken::Middleware
   class Client
     def call(options)
-    	options[:message_body]["apartment"] ||= Apartment::Tenant.current
+    	options[:message_body]["apartment"] = Apartment::Tenant.current
       yield
     end
   end
