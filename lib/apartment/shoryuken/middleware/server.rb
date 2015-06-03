c10b9ef9e1e5228ec87bf8b9c2e0367bc5e2c3c5
yurifrl <yurifrl@hotmail.com>
2015-06-03 11:57:36 -0300

    forgot fucking yeild

--- lib/apartment/shoryuken/middleware/server.rb
+++ lib/apartment/shoryuken/middleware/server.rb
@@ -2,6 +2,7 @@ module Apartment::Shoryuken::Middleware
   class Server
     def call(worker_instance, queue, sqs_msg, body)
       Apartment::Tenant.switch!(body['apartment'])
+      yield
     end
   end
 end
